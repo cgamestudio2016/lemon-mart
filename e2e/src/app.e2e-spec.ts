2009d83510ceeaadf26442dd6163dbef912e44bc
Tuyen NguyenDuy <duytuyen1988@gmail.com>
2020-10-27 23:08:01 +0700

    Fixed lint bugs.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,12 +1,10 @@
 import { browser, logging } from 'protractor'
 
-import { AppPage } from './app.po'
-
 describe('workspace-project App', () => {
-  let page: AppPage
+  // let page: AppPage
 
   beforeEach(() => {
-    page = new AppPage()
+    // page = new AppPage()
   })
 
   // it('should display welcome message', () => {
